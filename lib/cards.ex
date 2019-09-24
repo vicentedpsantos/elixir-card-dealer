41cbf60f8dfe3942600265b7db0897024f9eaa08
Vicente Santos <vicentedpsantos@gmail.com>
2019-09-23 21:12:15 -0300

    Finished cards module

--- lib/cards.ex
+++ lib/cards.ex
@@ -9,12 +9,31 @@ defmodule Cards do
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
-  
+
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
   def deal(deck, hand_size) do
-    Enum.split(deck, hand_size)
+    { hand, _rest_of_the_deck } = Enum.split(deck, hand_size)
+    hand
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      { :ok, binary } -> :erlang.binary_to_term(binary)
+      { :error, _reason } -> "That file does not exist" 
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
